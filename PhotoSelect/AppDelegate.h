9c1fbb42a808705993d0476103924c5e5811479a
Mopon <357491060@qq.com>
2016-04-28 15:27:04 +0800

    Initial Commit

---
+++ PhotoSelect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoSelect
+//
+//  Created by Mopon on 16/4/28.
+//  Copyright © 2016年 Mopon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoSelect/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PhotoSelect
+//
+//  Created by Mopon on 16/4/28.
+//  Copyright © 2016年 Mopon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
